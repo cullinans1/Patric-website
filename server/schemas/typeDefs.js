2786558fbe08406d4388bddf80aa40c736483ec8
Sammy <sammy.cullinan@gmail.com>
2020-11-27 12:06:24 -0700

    typedefs query finished

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,2 +1,28 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
+const typeDefs = gql`
+  type User {
+    _id: ID
+    name: String
+    email: String
+  }
+  type Auth {
+    token: ID
+    user: User
+  }
+  type Post {
+      _id: ID
+      title: String
+      image: String
+      content: String
+      link: String
+  }
+
+  type Query {
+      posts: [Post]
+  }
+
+  type Mutation{
+      
+  }
+`;
