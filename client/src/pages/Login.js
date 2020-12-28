590d5fee776a47a1084c2c8d3981e2c6b4762ad3
Sammy <sammy.cullinan@gmail.com>
2020-12-28 14:32:19 -0700

    login logic added

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useMutation } from "@apollo/react-hooks";
+import Auth from "../utils/auth";
+import { LOGIN } from "../utils/mutations";
 
-const Login = () => {
+function Login(props) {
+  const [formState, setFormState] = useState({ email: "", password: "" });
+  const [login, { error }] = useMutation(LOGIN);
 
-    
-    return (
-        <div>
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const mutationResponse = await login({
+        variables: {
+          email: formState.email,
+          password: formState.password,
+        },
+      });
+      const token = mutationResponse.data.login.token;
+      Auth.login(token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
 
-        </div>
-    )
+  return(
+      <div>
+          
+      </div>
+  )
 }
